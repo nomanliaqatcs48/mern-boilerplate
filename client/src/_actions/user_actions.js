cfbebc7cca6718b09ba55ddad2fb936703cbf46d
DarabMonib <darab.monib@invozone.com>
2022-08-04 17:00:29 +0500

    Added testing frameworks Features: - Mocha, Chai, Jest added - respective test directories added
    Turned on EsLint rules.
    Fixes:
    - Reopened Turned off rules for eslint
    - Removed index.js erronous files

--- client/src/_actions/user_actions.js
+++ client/src/_actions/user_actions.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import { LOGIN_USER, REGISTER_USER, AUTH_USER, LOGOUT_USER } from './types';
-import { USER_SERVER } from '../components/Config.js';
+const USER_SERVER = '/api/users';
 
 export function registerUser(dataToSubmit) {
   const request = axios
